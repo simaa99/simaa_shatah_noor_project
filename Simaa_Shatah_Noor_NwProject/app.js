f4046d654dfedd9cde30c54f6bf75191226553d6
simaa abumosa <semaamosa99@gmail.com>
2021-06-05 22:15:21 +0300

    NW_PROJECT

---
+++ Simaa_Shatah_Noor_NwProject/app.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
